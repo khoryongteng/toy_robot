1b1bb6f4674bdcfe555c4df8cef7c0146e3af98a
khoryongteng <yongtengkhor338@gmail.com>
2024-08-09 22:56:40 +0100

    Added Logic for Robot Placing

--- src/robot.py
+++ src/robot.py
@@ -1,10 +1,54 @@
 from command import Command, CommandType, Direction
 
 class Robot:
+    _min_x = 0
+    _max_x = 4
+    _min_y = 0
+    _max_y = 4
+    
     def __init__(self):
-        self.coordinates = None
-        self.direction = None
-        self.placed = False
+        self._x = None
+        self._y = None
+        self._direction = None
+        self._placed = False
+    
+    def _is_within_bounds(self, x: int, y: int) -> bool:
+        return self._min_x <= x <= self._max_x and self._min_y <= y <= self._max_y
         
-    def execute(self, command: Command):
+    def _place(self, x: int, y: int, direction: Direction):
+        if self._is_within_bounds(x, y):
+            self._x = x
+            self._y = y
+            self._direction = direction
+            self._placed = True
+    
+    def _move(self):
         pass
+    
+    def _left(self):
+        pass
+    
+    def _right(self):
+        pass
+    
+    def _report(self):
+        pass
+    
+    def execute(self, command: Command):
+        if command.command_type == CommandType.NULL:
+            return
+        
+        if command.command_type == CommandType.PLACE:
+            self._place(command.x, command.y, command.direction)
+        
+        if not self._placed:
+            return
+        
+        if command.command_type == CommandType.MOVE:
+            self._move()
+        elif command.command_type == CommandType.LEFT:
+            self._left()
+        elif command.command_type == CommandType.RIGHT:
+            self._right()
+        elif command.command_type == CommandType.REPORT:
+            self._report()
--- tests/test_robot.py
+++ tests/test_robot.py
@@ -1,10 +1,51 @@
 import pytest
 from src.robot import Robot
+from src.command import Command, CommandType, Direction
 
 @pytest.fixture
 def robot():
     return Robot()
  
 class TestRobot:
-    def test_xxx(self, robot):
-        assert isinstance(robot, Robot)
+    def test_robot_initialized_with_place_set_to_false(self):
+        robot = Robot()
+        assert robot._placed == False
+            
+    @pytest.mark.parametrize("place_command", [
+        Command(CommandType.PLACE, -1, 0, Direction.NORTH),
+        Command(CommandType.PLACE, 5, 0, Direction.SOUTH),
+        Command(CommandType.PLACE, 0, -1, Direction.EAST),
+        Command(CommandType.PLACE, 0, 5, Direction.WEST),
+    ])   
+    def test_place_invalid_coordinates_does_nothing(self, robot, place_command):
+        state_before = robot.__dict__.copy()
+        robot.execute(place_command)
+        state_after = robot.__dict__.copy()
+        assert state_before == state_after
+    
+    @pytest.mark.parametrize("place_command", [
+        Command(CommandType.PLACE, 0, 0, Direction.NORTH),
+        Command(CommandType.PLACE, 0, 4, Direction.SOUTH),
+        Command(CommandType.PLACE, 4, 0, Direction.EAST),
+        Command(CommandType.PLACE, 2, 3, Direction.WEST),
+    ])       
+    def test_place_with_valid_coordinates_sets_correct_values(self, robot, place_command):
+        robot.execute(place_command)
+        assert robot._placed == True
+        assert robot._x == place_command.x
+        assert robot._y == place_command.y
+        assert robot._direction == place_command.direction
+        
+    @pytest.mark.parametrize("place_command", [
+        Command(CommandType.PLACE, 0, 0, Direction.NORTH),
+        Command(CommandType.PLACE, 0, 4, Direction.SOUTH),
+        Command(CommandType.PLACE, 4, 0, Direction.EAST),
+        Command(CommandType.PLACE, 2, 3, Direction.WEST),
+    ])       
+    def test_place_with_valid_coordinates_sets_correct_values_even_when_already_placed(self, robot, place_command):
+        robot.execute(Command(CommandType.PLACE, 0, 0, Direction.NORTH))
+        robot.execute(place_command)
+        assert robot._placed == True
+        assert robot._x == place_command.x
+        assert robot._y == place_command.y
+        assert robot._direction == place_command.direction
